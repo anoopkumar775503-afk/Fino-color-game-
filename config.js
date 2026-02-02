2516ea7eb06318955697fe71726e82d1fbbafc34
anoopkumar775503-afk <anoopkumar775503@gmail.com>
2026-02-02 21:38:55 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDsAQlJNaWRK6xf7Kx1Ac8Y6hObuY8sBbw",
+  authDomain: "fino-color.firebaseapp.com",
+  projectId: "fino-color",
+  storageBucket: "fino-color.firebasestorage.app",
+  messagingSenderId: "686498750999",
+  appId: "1:686498750999:web:e21edc0746eeaab3067252"
+};
+
+// इसे विंडो ऑब्जेक्ट में डालें ताकि दूसरी फाइलें इसे इस्तेमाल कर सकें
+window.firebaseConfig = firebaseConfig;
